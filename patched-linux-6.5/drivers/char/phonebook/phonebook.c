153de3912ca81b1b2de9a7c8f05d864b137eea88
Arseny Staroverov <arsen2001god@bk.ru>
2024-03-04 17:24:22 +0300

    basic char device kinda works

--- patched-linux-6.5/drivers/char/phonebook/phonebook.c
+++ patched-linux-6.5/drivers/char/phonebook/phonebook.c
@@ -10,7 +10,11 @@ MODULE_AUTHOR("arseny staroverov.ag@phystech.ru");
 MODULE_DESCRIPTION("");
 MODULE_VERSION("0.0");
 
-static dev_t major;
+#define MY_MAJOR  266 // i had bad experience with `alloc_chrdev_region` so i decided to hardcode
+#define MY_MINOR  0
+#define MY_DEV_COUNT 1
+
+static dev_t devno = MKDEV(MY_MAJOR, MY_MINOR);
 
 struct cdev cdev;
 
@@ -36,6 +40,14 @@ static ssize_t device_read(struct file *file, char __user *buf, size_t count, lo
     uint8_t *data = "Hello from the kernel world!\n";
     size_t datalen = strlen(data);
 
+    if (datalen < *offset) {
+        pr_err("fiasco: offset overrun the content");
+        return 0;
+    }
+
+    data += *offset;
+    datalen = strlen(data);
+
     if (count > datalen) {
         count = datalen;
     }
@@ -43,6 +55,8 @@ static ssize_t device_read(struct file *file, char __user *buf, size_t count, lo
     if (copy_to_user(buf, data, count)) {
         return -EFAULT;
     }
+
+    *offset += count;
 
     return count;
 }
@@ -92,32 +106,34 @@ static int __init phonebook_init(void) {
     printk(KERN_INFO "Phonebook started\n");
 
     int rc = -EINVAL;
-    rc = alloc_chrdev_region(&major, 0, 1, PHONEBOOK_MODULE_NAME);
+
+    rc = register_chrdev_region(devno, MY_DEV_COUNT , PHONEBOOK_MODULE_NAME);
+    
+    // rc = alloc_chrdev_region(&devno, 0, 1, PHONEBOOK_MODULE_NAME); // sad
     if (rc) {
-		pr_err("Unable to allocate phonebook major number: %i\n", rc);
+		pr_err("Unable to create phonebook chrdev: %i\n", rc);
 		return rc;
 	}
     printk(KERN_INFO "region allocated\n");
-    major = MAJOR(major);
 
     phonebook_class = class_create(PHONEBOOK_MODULE_NAME);
     phonebook_class->dev_uevent = phonebook_uevent;
 
     cdev_init(&cdev, &phonebook_fops);
 
-    rc = cdev_add(&cdev, major, 1);
+    rc = cdev_add(&cdev, devno, 1);
 	if (rc) {
 		pr_err("cdev_add() failed %d\n", rc);
         return rc;
 	}
     printk(KERN_INFO "cdev_added\n");
 
-    phone_book_dev = device_create(phonebook_class, NULL, MKDEV(major, 0), NULL,
+    phone_book_dev = device_create(phonebook_class, NULL, devno, NULL,
 				    "%s-%d", PHONEBOOK_MODULE_NAME, 0);
 
     if (IS_ERR(phone_book_dev)) {
 		rc = PTR_ERR(phone_book_dev);
-		pr_err("Unable to create coproc-%d %d\n", MINOR(major), rc);
+		pr_err("Unable to create coproc-%d %d\n", MINOR(devno), rc);
         return rc;
 	}
 
