a5b4896bf76ec7c2d7c3605c82a620ea9a21fa0a
Arseny Staroverov <arsen2001god@bk.ru>
2024-03-09 11:26:40 +0300

    format code

--- linux-6.5/drivers/char/phonebook/phonebook.h
+++ linux-6.5/drivers/char/phonebook/phonebook.h
@@ -1,9 +1,16 @@
+#include <linux/types.h>
+
 #define MAX_USER_DATA_LEN ((size_t)32)
 
 struct user_data {
-    char surname[MAX_USER_DATA_LEN];
-    char name[MAX_USER_DATA_LEN];
-    int age;
-    char phone_number[MAX_USER_DATA_LEN];
-    char email[MAX_USER_DATA_LEN];
+  char surname[MAX_USER_DATA_LEN];
+  char name[MAX_USER_DATA_LEN];
+  int age;
+  char phone_number[MAX_USER_DATA_LEN];
+  char email[MAX_USER_DATA_LEN];
 };
+
+long __get_user_from_phonebook(const char *surname, unsigned int len,
+                               struct user_data *output_data);
+long __add_user_to_phonebook(const struct user_data *input_data);
+long __del_user_from_phonebook(const char *surname, unsigned int len);
